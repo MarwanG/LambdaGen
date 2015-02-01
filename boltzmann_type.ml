d7a33afebb6b7804cf9970e097e642246ad2151c
ghiles <ghilesZ>
2015-02-01 19:26:35 +0100

    ajout des listes

--- boltzmann_type.ml
+++ boltzmann_type.ml
@@ -2,13 +2,7 @@ open Types
 
 type type_tree = Node of type_tree * type_tree | Leaf
 
-let rec tree_to_term_type tree = 
-  match tree with
-  | Node (a,b) -> Fun_type (tree_to_term_type a, tree_to_term_type b)
-  | _ -> Const_type Int_type
-
 let b x = (1. -. sqrt (1. -. 4. *. x)) /. 2.
-
 
 let rec size_tree tree =
   match tree with 
@@ -20,28 +14,29 @@ let generate z =
     let node_proba = z /. (b z) in 
     let rec aux () =
       let x = Random.float 1. in
-	(*print_float x;
-	print_newline();*)
-      if x < node_proba then
-	begin
-	 (* print_string "leaf \n"; *)
-	  Leaf
-        end
-      else
-	begin
-	  (*print_string "node \n";*)
-	  Node (aux (),aux ())
-	end
+      if x < node_proba then Leaf else Node (aux (),aux ())
     in aux ()
   else 
     failwith "z must be in [0;0.25["
-      
+
+let rec tree_to_term_type tree = 
+  match tree with
+  | Node (a,b) -> Fun_type (tree_to_term_type a, tree_to_term_type b)
+  | _ ->
+    let x = Random.float 1. in
+    if x < 0.7 then Const_type Int_type 
+    else Const_type (List_type (tree_to_term_type (generate 0.22)))
+
 let gen_type z =
-  tree_to_term_type  (generate z)
+  tree_to_term_type (generate z)
 
 let rec improvise t =
   match t with
-  | Const_type Int_type -> Const (Int (Random.int 10))
+  | Const_type Int_type -> 
+    Const (Int (Random.int 10))
+      
+  | Const_type (List_type(a))-> 
+     Const (List [improvise a; improvise a;improvise a])
   | Fun_type (t1,t2) -> 
     try Environnement.search t
     with 
--- types.ml
+++ types.ml
@@ -1,6 +1,7 @@
 (* les differents types possibles *)
 type consttype = 
   Int_type
+| List_type of term_type
 and term_type = 
 | Const_type of consttype
 | Fun_type of term_type * term_type
@@ -14,7 +15,7 @@ type term_expr =
 |  App of term_expr * term_expr                 (* M N *)
 |  Abs of string * term_expr                    (* fun x -> M *)
 
-and term_const = Int of int
+and term_const = Int of int | List of term_expr list
 
 and unop = Moins
 
@@ -27,8 +28,17 @@ let rec string_of_type t =
 
 let rec string_of_exp t =
   match t with 
-    | Const (Int i) -> string_of_int i 
+    | Const (Int i) -> string_of_int i
+    | Const (List l) -> 
+      let rec aux l = 
+	match l with 
+	  [] -> ""
+	| h::[] -> (string_of_exp h)
+	| h::tl -> ((string_of_exp h)^"; "^(aux tl))
+      in ("["^(aux l)^"]")
     | Var s -> s            
     | App (t1,t2) ->  "("^(string_of_exp t1)^") ("^(string_of_exp t2)^")"          
     | Abs (s,t) ->  "(fun "^s^" -> "^(string_of_exp t)^")" 
-    | Binop (_,t1,t2) ->(string_of_exp t1)^" + "^(string_of_exp t2)    
+    | Binop (_,t1,t2) ->(string_of_exp t1)^" + "^(string_of_exp t2)
+    | Unop (_,t1) ->("- "^(string_of_exp t1) )   
+
